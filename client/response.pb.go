0126298a1758b7f72f5a412dc4558660c3ee3bc3
sefaphlvn <sefa.pehlivan@hepsiburada.com>
2025-05-26 21:32:37 +0300

    fix

--- client/response.pb.go
+++ client/response.pb.go
@@ -503,6 +503,51 @@ func (x *ResponseEnvoyAdmin) GetHeaders() map[string]string {
 	return nil
 }
 
+type ResponseClientLog struct {
+	state         protoimpl.MessageState
+	sizeCache     protoimpl.SizeCache
+	unknownFields protoimpl.UnknownFields
+
+	Logs []*Logs `protobuf:"bytes,1,rep,name=logs,proto3" json:"logs,omitempty"`
+}
+
+func (x *ResponseClientLog) Reset() {
+	*x = ResponseClientLog{}
+	mi := &file_client_response_proto_msgTypes[7]
+	ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+	ms.StoreMessageInfo(mi)
+}
+
+func (x *ResponseClientLog) String() string {
+	return protoimpl.X.MessageStringOf(x)
+}
+
+func (*ResponseClientLog) ProtoMessage() {}
+
+func (x *ResponseClientLog) ProtoReflect() protoreflect.Message {
+	mi := &file_client_response_proto_msgTypes[7]
+	if x != nil {
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		if ms.LoadMessageInfo() == nil {
+			ms.StoreMessageInfo(mi)
+		}
+		return ms
+	}
+	return mi.MessageOf(x)
+}
+
+// Deprecated: Use ResponseClientLog.ProtoReflect.Descriptor instead.
+func (*ResponseClientLog) Descriptor() ([]byte, []int) {
+	return file_client_response_proto_rawDescGZIP(), []int{7}
+}
+
+func (x *ResponseClientLog) GetLogs() []*Logs {
+	if x != nil {
+		return x.Logs
+	}
+	return nil
+}
+
 var File_client_response_proto protoreflect.FileDescriptor
 
 var file_client_response_proto_rawDesc = []byte{
@@ -569,11 +614,14 @@ var file_client_response_proto_rawDesc = []byte{
 	0x1a, 0x3a, 0x0a, 0x0c, 0x48, 0x65, 0x61, 0x64, 0x65, 0x72, 0x73, 0x45, 0x6e, 0x74, 0x72, 0x79,
 	0x12, 0x10, 0x0a, 0x03, 0x6b, 0x65, 0x79, 0x18, 0x01, 0x20, 0x01, 0x28, 0x09, 0x52, 0x03, 0x6b,
 	0x65, 0x79, 0x12, 0x14, 0x0a, 0x05, 0x76, 0x61, 0x6c, 0x75, 0x65, 0x18, 0x02, 0x20, 0x01, 0x28,
-	0x09, 0x52, 0x05, 0x76, 0x61, 0x6c, 0x75, 0x65, 0x3a, 0x02, 0x38, 0x01, 0x42, 0x2d, 0x5a, 0x2b,
-	0x67, 0x69, 0x74, 0x68, 0x75, 0x62, 0x2e, 0x63, 0x6f, 0x6d, 0x2f, 0x45, 0x6c, 0x63, 0x68, 0x69,
-	0x54, 0x68, 0x65, 0x47, 0x72, 0x65, 0x61, 0x74, 0x2f, 0x65, 0x6c, 0x63, 0x68, 0x69, 0x2d, 0x70,
-	0x72, 0x6f, 0x74, 0x6f, 0x2f, 0x63, 0x6c, 0x69, 0x65, 0x6e, 0x74, 0x62, 0x06, 0x70, 0x72, 0x6f,
-	0x74, 0x6f, 0x33,
+	0x09, 0x52, 0x05, 0x76, 0x61, 0x6c, 0x75, 0x65, 0x3a, 0x02, 0x38, 0x01, 0x22, 0x35, 0x0a, 0x11,
+	0x52, 0x65, 0x73, 0x70, 0x6f, 0x6e, 0x73, 0x65, 0x43, 0x6c, 0x69, 0x65, 0x6e, 0x74, 0x4c, 0x6f,
+	0x67, 0x12, 0x20, 0x0a, 0x04, 0x6c, 0x6f, 0x67, 0x73, 0x18, 0x01, 0x20, 0x03, 0x28, 0x0b, 0x32,
+	0x0c, 0x2e, 0x63, 0x6c, 0x69, 0x65, 0x6e, 0x74, 0x2e, 0x4c, 0x6f, 0x67, 0x73, 0x52, 0x04, 0x6c,
+	0x6f, 0x67, 0x73, 0x42, 0x2d, 0x5a, 0x2b, 0x67, 0x69, 0x74, 0x68, 0x75, 0x62, 0x2e, 0x63, 0x6f,
+	0x6d, 0x2f, 0x45, 0x6c, 0x63, 0x68, 0x69, 0x54, 0x68, 0x65, 0x47, 0x72, 0x65, 0x61, 0x74, 0x2f,
+	0x65, 0x6c, 0x63, 0x68, 0x69, 0x2d, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x2f, 0x63, 0x6c, 0x69, 0x65,
+	0x6e, 0x74, 0x62, 0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
 }
 
 var (
@@ -588,7 +636,7 @@ func file_client_response_proto_rawDescGZIP() []byte {
 	return file_client_response_proto_rawDescData
 }
 
-var file_client_response_proto_msgTypes = make([]protoimpl.MessageInfo, 8)
+var file_client_response_proto_msgTypes = make([]protoimpl.MessageInfo, 9)
 var file_client_response_proto_goTypes = []any{
 	(*ResponseDeploy)(nil),          // 0: client.ResponseDeploy
 	(*ResponseUnDeploy)(nil),        // 1: client.ResponseUnDeploy
@@ -597,17 +645,19 @@ var file_client_response_proto_goTypes = []any{
 	(*ServiceStatus)(nil),           // 4: client.ServiceStatus
 	(*Logs)(nil),                    // 5: client.Logs
 	(*ResponseEnvoyAdmin)(nil),      // 6: client.ResponseEnvoyAdmin
-	nil,                             // 7: client.ResponseEnvoyAdmin.HeadersEntry
+	(*ResponseClientLog)(nil),       // 7: client.ResponseClientLog
+	nil,                             // 8: client.ResponseEnvoyAdmin.HeadersEntry
 }
 var file_client_response_proto_depIdxs = []int32{
 	4, // 0: client.ResponseService.status:type_name -> client.ServiceStatus
 	5, // 1: client.ResponseService.logs:type_name -> client.Logs
-	7, // 2: client.ResponseEnvoyAdmin.headers:type_name -> client.ResponseEnvoyAdmin.HeadersEntry
-	3, // [3:3] is the sub-list for method output_type
-	3, // [3:3] is the sub-list for method input_type
-	3, // [3:3] is the sub-list for extension type_name
-	3, // [3:3] is the sub-list for extension extendee
-	0, // [0:3] is the sub-list for field type_name
+	8, // 2: client.ResponseEnvoyAdmin.headers:type_name -> client.ResponseEnvoyAdmin.HeadersEntry
+	5, // 3: client.ResponseClientLog.logs:type_name -> client.Logs
+	4, // [4:4] is the sub-list for method output_type
+	4, // [4:4] is the sub-list for method input_type
+	4, // [4:4] is the sub-list for extension type_name
+	4, // [4:4] is the sub-list for extension extendee
+	0, // [0:4] is the sub-list for field type_name
 }
 
 func init() { file_client_response_proto_init() }
@@ -621,7 +671,7 @@ func file_client_response_proto_init() {
 			GoPackagePath: reflect.TypeOf(x{}).PkgPath(),
 			RawDescriptor: file_client_response_proto_rawDesc,
 			NumEnums:      0,
-			NumMessages:   8,
+			NumMessages:   9,
 			NumExtensions: 0,
 			NumServices:   0,
 		},
